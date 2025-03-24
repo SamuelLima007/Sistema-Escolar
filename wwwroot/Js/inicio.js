8f0bfa90f54884e237ed81ba7f5a51a071685932
Samuel <samuelimagg@gmail.com>
2025-03-23 23:20:22 -0300

    atualizacoes

--- wwwroot/Js/inicio.js
+++ wwwroot/Js/inicio.js
@@ -1,26 +1,29 @@
-// var janela = document.querySelector(".janela-modal");
-// var botao = document.querySelector(".btn");
-// var body = document.querySelector("body");
+var janela = document.querySelector(".janela-modal");
+var botao = document.querySelector(".btn");
+var body = document.querySelector("body");
+var dashboard = document.querySelector(".dashboard");
 
-// function abrirmodal() {
-//   janela.style.display = "flex";
-//   body.classList.add("background-transparente");
-// }
+function abrirmodal() {
+  janela.style.display = "flex";
 
-// function fecharmodal() {
-//   janela.style.display = "none";
-//   body.classList.remove("background-transparente");
-// }
+  body.classList.add("background-transparente");
+}
 
-// botao.addEventListener("click", fecharmodal);
+function fecharmodal() {
+  janela.style.display = "none";
+  body.classList.remove("background-transparente");
+  dashboard.style.display = "flex";
+}
 
-// document.addEventListener("DOMContentLoaded", abrirmodal);
+botao.addEventListener("click", fecharmodal);
+
+document.addEventListener("DOMContentLoaded", abrirmodal);
 
 const hora = document.querySelector(".Hora");
 
 function AtualizarHora() {
   const horario = new Date();
-  const horarioformatado = horario.toLocaleDateString("pt-BR");
+  const horarioformatado = horario.toLocaleTimeString("pt-BR");
   hora.textContent = `${horarioformatado}`;
 }
 
