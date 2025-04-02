b9555ab8a87022fb56a38fb1dceaf6b6a293e066
Samuel <samuelimagg@gmail.com>
2025-04-02 18:58:32 -0300

    att

--- Domain/ViewModels/CreateAdministratorViewModel.cs
+++ Domain/ViewModels/CreateAdministratorViewModel.cs
@@ -13,9 +13,9 @@ namespace ProjetoScores.Domain.ViewModels
         [MaxLength(100, ErrorMessage = "O name deve ter no máximo 100 caracteres")]
         public string? Name { get; set; }
         public string? Email { get; set; }
-        [Required(ErrorMessage = "A password é obrigatória")]
-        [MinLength(6, ErrorMessage = "A password deve ter no mínimo 6 caracteres")]
-        [MaxLength(100, ErrorMessage = "A password deve ter no máximo 100 caracteres")]
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        [MaxLength(100, ErrorMessage = "A senha deve ter no máximo 100 caracteres")]
         public string? Password { get; set; }
         public string? Roles { get; set; } = "admin";
     }
