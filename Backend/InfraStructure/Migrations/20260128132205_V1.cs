c7c91f8478d3a7c852c4b58e09388093b0f27e05
Samuel Lima da Silva <samuel.lsilva@grupomateus.com>
2026-01-30 17:13:39 -0300

    Estilizando tela de login

---
+++ Backend/InfraStructure/Migrations/20260128132205_V1.cs
@@ -0,0 +1,253 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ProjetoNotas.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    FotoPerfil = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Role = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    ClassId = table.Column<int>(type: "integer", nullable: true),
+                    Score1 = table.Column<int>(type: "integer", nullable: true),
+                    Score2 = table.Column<int>(type: "integer", nullable: true),
+                    Score3 = table.Column<int>(type: "integer", nullable: true),
+                    Score4 = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Class",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Grade = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Class", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Class_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subject",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subject_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Unit = table.Column<int>(type: "integer", nullable: false),
+                    score = table.Column<int>(type: "integer", nullable: false),
+                    CreationDate = table.Column<string>(type: "character varying(48)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "character varying(48)", nullable: false),
+                    SubjectId = table.Column<int>(type: "integer", nullable: false),
+                    ClassId = table.Column<int>(type: "integer", nullable: false),
+                    TeacherId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Task_Class_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Class",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Task_Subject_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subject",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Task_Users_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeacherAssignments",
+                columns: table => new
+                {
+                    TeacherId = table.Column<int>(type: "integer", nullable: false),
+                    ClassId = table.Column<int>(type: "integer", nullable: false),
+                    SubjectId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeacherAssignments", x => new { x.TeacherId, x.ClassId, x.SubjectId });
+                    table.ForeignKey(
+                        name: "FK_TeacherAssignments_Class_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Class",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TeacherAssignments_Subject_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subject",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TeacherAssignments_Users_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubmittedTask",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    MyTaskId = table.Column<int>(type: "integer", nullable: false),
+                    Score = table.Column<decimal>(type: "numeric", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubmittedTask", x => new { x.StudentId, x.MyTaskId });
+                    table.ForeignKey(
+                        name: "FK_SubmittedTask_Task_MyTaskId",
+                        column: x => x.MyTaskId,
+                        principalTable: "Task",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SubmittedTask_Users_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Class_UserId",
+                table: "Class",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subject_Name",
+                table: "Subject",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subject_UserId",
+                table: "Subject",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubmittedTask_MyTaskId",
+                table: "SubmittedTask",
+                column: "MyTaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubmittedTask_StudentId_MyTaskId",
+                table: "SubmittedTask",
+                columns: new[] { "StudentId", "MyTaskId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_ClassId",
+                table: "Task",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_SubjectId",
+                table: "Task",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_TeacherId",
+                table: "Task",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherAssignments_ClassId",
+                table: "TeacherAssignments",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherAssignments_SubjectId",
+                table: "TeacherAssignments",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherAssignments_TeacherId_ClassId_SubjectId",
+                table: "TeacherAssignments",
+                columns: new[] { "TeacherId", "ClassId", "SubjectId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubmittedTask");
+
+            migrationBuilder.DropTable(
+                name: "TeacherAssignments");
+
+            migrationBuilder.DropTable(
+                name: "Task");
+
+            migrationBuilder.DropTable(
+                name: "Class");
+
+            migrationBuilder.DropTable(
+                name: "Subject");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
