31c69f40871fe0299e7b178da90600f119fd67ad
Samuel Lima da Silva <samuel.lsilva@grupomateus.com>
2026-01-31 17:03:08 -0300

    Validações dos campos da tela de login

--- frontend/src/app/Pages/login/login.ts
+++ frontend/src/app/Pages/login/login.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SharedModule } from '../../shared-module/shared/shared-module';
 import { LogoIcon } from '../../Icons/logo-icon/logo-icon';
 import { GraduationCap } from 'lucide-angular/src/icons';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,5 +12,28 @@ import { GraduationCap } from 'lucide-angular/src/icons';
   styleUrl: './login.css',
 })
 export class Login {
+  private formBuilder = inject(FormBuilder);
+
+  loginForm = this.formBuilder.group({
+    email: [
+      '',
+      Validators.required,
+      Validators.pattern(
+        '^[a-zA-Z0-9.!#$%&&#39;*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*$',
+      ),
+    ],
+    password: ['', Validators.required, Validators.minLength(12) ],
+  });
   readonly GraduationCap = GraduationCap;
+
+
+  onSubmit()
+  {
+    if (this.loginForm.valid)
+    {
+
+    }
+    
+   
+  }
 }
--- frontend/src/app/shared-module/shared/shared-module.ts
+++ frontend/src/app/shared-module/shared/shared-module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule } from 'lucide-angular';
+import { MessageModule } from 'primeng/message'
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [CommonModule, LucideAngularModule],
+  exports: [CommonModule, LucideAngularModule, MessageModule, ReactiveFormsModule, RouterModule],
 })
 export class SharedModule {}
